211042837701d79e20f2c87c7f3e608421789ee0
Joy Markarian <jmarkarian@hotmail.fr>
2023-03-30 11:18:33 +0200

    Twig_Quest_3

--- public/products.php
+++ public/products.php
@@ -4,9 +4,6 @@ require_once __DIR__ . '/../config/twig.php';
 
 $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
-$twig = new \Twig\Environment($loader);
-
 echo $twig->render('products.html.twig', [
     'products' => $products
 ]);
